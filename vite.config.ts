71f6cc8c15757b2d257c2fe84188df88d572ed97
Gerd Kortemeyer <kgerd@ethz.ch>
2025-06-15 21:22:52 +0200

    Switching to SSL

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  }
+export default defineConfig(({ command }) => {
+  const isServe = command === 'serve';
+  return {
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+      https: isServe
+        ? {
+            key: fs.readFileSync('/run/certs/privkey.pem'),
+            cert: fs.readFileSync('/run/certs/fullchain.pem'),
+          }
+        : false,
+    },
+  };
 });
+
